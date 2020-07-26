4844f7bae5f6ccff98f1e0cbbbd517141d7ef19e
Mayank Kyal <49616985+mayankyal@users.noreply.github.com>
2020-07-26 17:18:02 +0530

    Update settings.py

--- VideoKyc/settings.py
+++ VideoKyc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a9im@+3k_$8n)o4-up1pdt2%87uvyfp3@%^plsvf43r)_x5a8_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
